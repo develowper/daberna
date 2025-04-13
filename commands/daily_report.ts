9943f7fecdbb8da874bc045e702217f65ba3299a
MMR <moj2raj2@gmail.com>
2025-04-14 02:07:30 +0330

    init adonis

--- commands/daily_report.ts
+++ commands/daily_report.ts
@@ -21,10 +21,10 @@ export default class DailyReport extends BaseCommand {
   static reportTime = DateTime.fromObject({ hour: 2, minute: 0 }, { zone: 'Asia/Tehran' })
   async run() {
     const now = DateTime.now().setZone('Asia/Tehran')
-    if (now.hour !== DailyReport.reportTime.hour || now.minute !== DailyReport.reportTime.minute) {
-      process.exit()
-      return
-    }
+    // if (now.hour !== DailyReport.reportTime.hour || now.minute !== DailyReport.reportTime.minute) {
+    //   process.exit()
+    //   return
+    // }
 
     const clearPeriodDay = (await getSettings('clear_period_day')) ?? 0
 
@@ -144,10 +144,10 @@ export default class DailyReport extends BaseCommand {
       acc[`today_card_${type}_count`] = 0
       return acc
     }, {})
-    // await Telegram.sendMessage(`${Helper.TELEGRAM_LOGS[0]}`, msg)
+    await Telegram.sendMessage(`${Helper.TELEGRAM_LOGS[0]}`, msg)
     // await Telegram.sendMessage(`${Helper.TELEGRAM_LOGS[1]}`, msg)
-    await Telegram.logAdmins(msg, null, Helper.TELEGRAM_TOPICS.STATISTICS)
-    await User.query().update({ ...zeroTodayData, todayPrize: 0 })
+    // await Telegram.logAdmins(msg, null, Helper.TELEGRAM_TOPICS.STATISTICS)
+    // await User.query().update({ ...zeroTodayData, todayPrize: 0 })
 
     // try {
 
