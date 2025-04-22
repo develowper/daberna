03be52fbc5114f5399401516559dc9be0e0921c0
MMR <moj2raj2@gmail.com>
2025-04-22 14:28:53 +0330

    init adonis

--- app/controllers/api/room_controller.ts
+++ app/controllers/api/room_controller.ts
@@ -47,96 +47,226 @@ export default class RoomController {
     return response.json({ data: data.map((item) => item.serialize()) })
   }
 
+  // async payAndJoin({ response, request, auth, i18n }: HttpContext) {
+  //   const user = auth.user as User
+  //   const roomType = request.input('room_type')
+  //   const cardCount = Number.parseInt(request.input('card_count'))
+  //   const ip = request.ip()
+  //   const trx = await db.transaction()
+  //
+  //   const room = await Room.query({ client: trx })
+  //     .where('is_active', true)
+  //     .where('type', roomType)
+  //     .forUpdate()
+  //     .first()
+  //
+  //   if (!user.isActive) {
+  //     // await trx.commit()
+  //     return response
+  //       .status(Helper.ERROR_STATUS)
+  //       .json({ message: i18n.t('messages.is_inactive_*', { item: i18n.t('messages.user') }) })
+  //   }
+  //   if (!room || Number.isNaN(cardCount)) {
+  //     // await trx.commit()
+  //     return response
+  //       .status(Helper.ERROR_STATUS)
+  //       .json({ message: i18n.t('messages.not_found_*', { item: i18n.t('messages.game_room') }) })
+  //   }
+  //   // const settings = await  getSettings([
+  //   //   'max_user_room_cards',
+  //   //   'max_room_cards',
+  //   //   'row_win_percent',
+  //   //   'win_percent',
+  //   // ])
+  //
+  //   const userBeforeCardCounts = room.getUserCardCount()
+  //
+  //   if (userBeforeCardCounts + cardCount > room.maxUserCardsCount) {
+  //     // await trx.commit()
+  //     return sendError(i18n.t('messages.validate.max_cards', { value: room.maxUserCardsCount }))
+  //   }
+  //   if (room.cardCount + cardCount > room.maxCardsCount) {
+  //     // await trx.commit()
+  //     return sendError(i18n.t('messages.validate.max_room_cards', { value: room.maxCardsCount }))
+  //   }
+  //   const beforeIpExists = collect(JSON.parse(room.players) ?? []).first(
+  //     (item: any) => ip != null && item.user_ip == ip && item.user_id != user.id
+  //   )
+
+  //   if (beforeIpExists) {
+  //     // await trx.commit()
+  //     return sendError(i18n.t('messages.validate.duplicate_*', { value: 'ip' }))
+  //   }
+  //
+  //   const userFinancials = await UserFinancial.firstOrCreate(
+  //     { userId: user?.id },
+  //     { balance: 0 } /*,
+  //     { client: trx }*/
+  //   )
+  //   const totalPrice = room.cardPrice * cardCount
+  //   // vine.compile(vine.object({
+  //   //   totalPrice: vine.number().max(userFinancials.balance)
+  //   // },)).validate({ totalPrice })
+  //
+  //   if (userFinancials.balance < totalPrice) {
+  //     // await trx.commit()
+  //     // userFinancials.balance = 100000
+  //     // userFinancials.save()
+  //     return sendError(
+  //       i18n.t('messages.validate.min', {
+  //         item: i18n.t('messages.wallet'),
+  //         value: `${asPrice(totalPrice)} ${i18n.t('messages.currency')}`,
+  //       })
+  //     )
+  //   }
+  //   try {
+  //     if (room.setUserCardsCount(userBeforeCardCounts + cardCount, user, request.ip())) {
+  //       if (userBeforeCardCounts == 0) {
+  //         room.playerCount++
+  //         user.playCount++
+  //       }
+  //       room.cardCount += cardCount
+  //
+  //       if (
+  //         room.playerCount == 2 &&
+  //         userBeforeCardCounts == 0 /*||
+  //       (room.playerCount > 2 && room.secondsRemaining == room.maxSeconds)*/
+  //       )
+  //         room.startAt = DateTime.now().plus({ seconds: room.maxSeconds - 1 })
+  //
+  //       await room.save()
+  //       // await room.useTransaction(trx).save()
+  //
+  //       userFinancials.balance -= totalPrice
+  //       // await userFinancials.useTransaction(trx).save()
+  //       await userFinancials.save()
+  //
+  //       switch (room.cardPrice) {
+  //         case 5000:
+  //           user.card5000Count += cardCount
+  //           user.todayCard5000Count += cardCount
+  //           break
+  //         case 10000:
+  //           user.card10000Count += cardCount
+  //           user.todayCard10000Count += cardCount
+  //           break
+  //         case 20000:
+  //           user.card20000Count += cardCount
+  //           user.todayCard20000Count += cardCount
+  //           break
+  //         case 50000:
+  //           user.card50000Count += cardCount
+  //           user.todayCard50000Count += cardCount
+  //           break
+  //       }
+  //
+  //       await user.save()
+  //
+  //       emitter.emit('room-update', {
+  //         type: roomType,
+  //         cmnd: 'card-added',
+  //         game_id: room.clearCount,
+  //         cards: room.cardCount,
+  //         players: room.players,
+  //         start_with_me: room.startWithMe,
+  //         seconds_remaining: room.playerCount > 1 ? room.secondsRemaining : room.maxSeconds,
+  //         player_count: room.playerCount,
+  //         // user_id: user?.id,
+  //         // username: user?.username,
+  //         // user_card_count: userBeforeCardCounts + cardCount,
+  //         card_count: room.cardCount,
+  //       })
+  //       // emitter.emit(`user-${user.id}-info`, { user_balance: userFinancials.balance })
+  //
+  //       // Daberna.startRooms([room])
+  //     }
+  //     // await trx.commit()
+  //   } catch (error) {
+  //     // await trx.rollback()
+  //   }
+  //   return response.json({ user_balance: userFinancials?.balance })
+  // }
+
   async payAndJoin({ response, request, auth, i18n }: HttpContext) {
     const user = auth.user as User
     const roomType = request.input('room_type')
     const cardCount = Number.parseInt(request.input('card_count'))
     const ip = request.ip()
-    // const trx = await db.transaction()
-    const room = await Room.query(/*{ client: trx }*/)
-      .where('is_active', true)
-      .where('type', roomType)
-      .first()
+    const trx = await db.transaction()
 
-    if (!user.isActive) {
-      // await trx.commit()
-      return response
-        .status(Helper.ERROR_STATUS)
-        .json({ message: i18n.t('messages.is_inactive_*', { item: i18n.t('messages.user') }) })
-    }
-    if (!room || Number.isNaN(cardCount)) {
-      // await trx.commit()
-      return response
-        .status(Helper.ERROR_STATUS)
-        .json({ message: i18n.t('messages.not_found_*', { item: i18n.t('messages.game_room') }) })
-    }
-    // const settings = await  getSettings([
-    //   'max_user_room_cards',
-    //   'max_room_cards',
-    //   'row_win_percent',
-    //   'win_percent',
-    // ])
-
-    const userBeforeCardCounts = room.getUserCardCount()
-
-    if (userBeforeCardCounts + cardCount > room.maxUserCardsCount) {
-      // await trx.commit()
-      return sendError(i18n.t('messages.validate.max_cards', { value: room.maxUserCardsCount }))
-    }
-    if (room.cardCount + cardCount > room.maxCardsCount) {
-      // await trx.commit()
-      return sendError(i18n.t('messages.validate.max_room_cards', { value: room.maxCardsCount }))
-    }
-    const beforeIpExists = collect(JSON.parse(room.players) ?? []).first(
-      (item: any) => ip != null && item.user_ip == ip && item.user_id != user.id
-    )
-    if (beforeIpExists) {
-      // await trx.commit()
-      return sendError(i18n.t('messages.validate.duplicate_*', { value: 'ip' }))
-    }
-
-    const userFinancials = await UserFinancial.firstOrCreate(
-      { userId: user?.id },
-      { balance: 0 } /*,
-      { client: trx }*/
-    )
-    const totalPrice = room.cardPrice * cardCount
-    // vine.compile(vine.object({
-    //   totalPrice: vine.number().max(userFinancials.balance)
-    // },)).validate({ totalPrice })
-
-    if (userFinancials.balance < totalPrice) {
-      // await trx.commit()
-      // userFinancials.balance = 100000
-      // userFinancials.save()
-      return sendError(
-        i18n.t('messages.validate.min', {
-          item: i18n.t('messages.wallet'),
-          value: `${asPrice(totalPrice)} ${i18n.t('messages.currency')}`,
-        })
-      )
-    }
     try {
-      if (room.setUserCardsCount(userBeforeCardCounts + cardCount, user, request.ip())) {
-        if (userBeforeCardCounts == 0) {
+      const room = await Room.query({ client: trx })
+        .where('is_active', true)
+        .where('type', roomType)
+        .forUpdate()
+        .first()
+
+      if (!user.isActive || !room || Number.isNaN(cardCount)) {
+        await trx.rollback()
+        return response.status(422).json({
+          message: i18n.t('messages.validation_failed'),
+        })
+      }
+
+      const userBeforeCardCounts = room.getUserCardCount()
+
+      if (userBeforeCardCounts + cardCount > room.maxUserCardsCount) {
+        await trx.rollback()
+        return response.status(400).json({
+          message: i18n.t('messages.validate.max_cards', { value: room.maxUserCardsCount }),
+        })
+      }
+
+      if (room.cardCount + cardCount > room.maxCardsCount) {
+        await trx.rollback()
+        return response.status(400).json({
+          message: i18n.t('messages.validate.max_room_cards', { value: room.maxCardsCount }),
+        })
+      }
+
+      const beforeIpExists = collect(JSON.parse(room.players) ?? []).first(
+        (item: any) => !!ip && item.user_ip === ip && item.user_id !== user.id
+      )
+      if (beforeIpExists) {
+        await trx.rollback()
+        return response.status(400).json({
+          message: i18n.t('messages.validate.duplicate_*', { value: 'ip' }),
+        })
+      }
+
+      const userFinancials = await UserFinancial.firstOrCreate(
+        { userId: user?.id },
+        { balance: 0 },
+        { client: trx }
+      )
+      const totalPrice = room.cardPrice * cardCount
+
+      if (userFinancials.balance < totalPrice) {
+        await trx.rollback()
+        return response.status(400).json({
+          message: i18n.t('messages.validate.min', {
+            item: i18n.t('messages.wallet'),
+            value: `${asPrice(totalPrice)} ${i18n.t('messages.currency')}`,
+          }),
+        })
+      }
+
+      if (room.setUserCardsCount(userBeforeCardCounts + cardCount, user, ip)) {
+        if (userBeforeCardCounts === 0) {
           room.playerCount++
           user.playCount++
         }
+
         room.cardCount += cardCount
 
-        if (
-          room.playerCount == 2 &&
-          userBeforeCardCounts == 0 /*||
-        (room.playerCount > 2 && room.secondsRemaining == room.maxSeconds)*/
-        )
+        if (room.playerCount === 2 && userBeforeCardCounts === 0) {
           room.startAt = DateTime.now().plus({ seconds: room.maxSeconds - 1 })
+        }
 
-        await room.save()
-        // await room.useTransaction(trx).save()
+        await room.useTransaction(trx).save()
 
         userFinancials.balance -= totalPrice
-        // await userFinancials.useTransaction(trx).save()
-        await userFinancials.save()
+        await userFinancials.useTransaction(trx).save()
 
         switch (room.cardPrice) {
           case 5000:
@@ -157,7 +287,9 @@ export default class RoomController {
             break
         }
 
-        await user.save()
+        await user.useTransaction(trx).save()
+
+        await trx.commit()
 
         emitter.emit('room-update', {
           type: roomType,
@@ -168,19 +300,17 @@ export default class RoomController {
           start_with_me: room.startWithMe,
           seconds_remaining: room.playerCount > 1 ? room.secondsRemaining : room.maxSeconds,
           player_count: room.playerCount,
-          // user_id: user?.id,
-          // username: user?.username,
-          // user_card_count: userBeforeCardCounts + cardCount,
           card_count: room.cardCount,
         })
-        // emitter.emit(`user-${user.id}-info`, { user_balance: userFinancials.balance })
 
-        // Daberna.startRooms([room])
+        return response.json({ user_balance: userFinancials.balance })
       }
-      // await trx.commit()
+
+      await trx.rollback()
+      return response.status(500).json({ message: 'Unexpected error occurred' })
     } catch (error) {
-      // await trx.rollback()
+      await trx.rollback()
+      return response.status(500).json({ message: 'Transaction failed', error })
     }
-    return response.json({ user_balance: userFinancials?.balance })
   }
 }
