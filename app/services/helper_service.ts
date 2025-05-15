24cb005e2d66d2ca2f96ddd7f45854191f924c2a
MMR <moj2raj2@gmail.com>
2025-05-15 12:35:47 +0330

    init adonis

--- app/services/helper_service.ts
+++ app/services/helper_service.ts
@@ -218,7 +218,6 @@ class Helper {
   public static SUPPORT = {
     telegram: 'https://t.me/paris_admin1',
   }
-
   public static SOCKET_LINK = `https://${env.get('APP_URL')}` ?? 'http://127.0.0.1:3298'
   /*'http://172.16.6.2:3298' ??*/
   public static SOCKET_LINK_CLIENT = `https://${env.get('APP_URL')}` /* ?? '127.0.0.1:3298' ?? ''*/
