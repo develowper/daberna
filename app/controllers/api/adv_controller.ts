78f50047c55c3aa5fa2a56533ea68a9beb684f7c
MMR <moj2raj2@gmail.com>
2025-05-16 02:56:41 +0330

    init adonis

--- app/controllers/api/adv_controller.ts
+++ app/controllers/api/adv_controller.ts
@@ -17,12 +17,10 @@ export default class UserController {
     const id = request.input('id')
 
     const ads = collect(JSON.parse((await Setting.findBy('key', 'ads'))?.value ?? '[]'))
-    console.log(ads)
     const updated = ads.map((item) => ({
       ...item,
       clicks: `${item.id}` === `${id}` ? item.clicks + 1 : item.clicks,
     }))
-    console.log(updated.all())
     await Setting.query()
       .where('key', 'ads')
       .update({ value: JSON.stringify(updated.all()) })
