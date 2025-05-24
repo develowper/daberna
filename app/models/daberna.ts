a2fe189386e7e8e706725000a83089f29d72c85d
MMR <moj2raj2@gmail.com>
2025-05-24 23:16:42 +0330

    init adonis

--- app/models/daberna.ts
+++ app/models/daberna.ts
@@ -450,10 +450,9 @@ export default class Daberna extends BaseModel {
       //redis decrement temp debit for this game
       const val = await redis.get(`b${user.id}`)
       let debit = (!Number.isNaN(Number(val)) ? Number(val) : 0) - buy
-      debit = debit < 0 ? 0 : debit
-      await redis.set(`b${user.id}`, debit)
-
-      l += `userId:${user.id}(${user.username}) buy:${buy} [${from}-${to}] debit:${debit} \n`
+      await redis.set(`b${user.id}`, debit < 0 ? 0 : debit)
+      const ttl = await redis.ttl(`b${user.id}`)
+      l += `userId:${user.id}(${user.username}) buy:${buy} [${from}-${to}] debit:${debit} ttl:(${ttl}) \n`
     }
     // await redis.del(`in${room.type}`)
 
