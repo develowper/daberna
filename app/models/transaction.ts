3e2256bf522d529fcd3d460f8679fabb3b003199
MMR <moj2raj2@gmail.com>
2025-08-17 22:55:19 +0330

    init adonis

--- app/models/transaction.ts
+++ app/models/transaction.ts
@@ -127,7 +127,7 @@ export default class Transaction extends BaseModel {
                 merchant: gateway?.value,
                 amount: `${price}0`,
                 // callbackUrl: `https://${Env.get('APP_URL')}/api/payment/done`,
-                callbackUrl: `https://shopsheek.ir/api/payment/done`,
+                callbackUrl: `https://daberna.shopsheek.ir/api/payment/done`,
                 description: `خریدار: ${payerName}`,
                 mobile: phone,
                 email: mail,
