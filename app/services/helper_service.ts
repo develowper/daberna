515cd32d52b018227330f2406753dcaefa4129d4
MMR <moj2raj2@gmail.com>
2025-08-17 14:36:12 +0330

    init adonis

--- app/services/helper_service.ts
+++ app/services/helper_service.ts
@@ -232,7 +232,7 @@ class Helper {
   public static ENAMAD = `<a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=565455&Code=WNIwA9GN3WFa1TNq7pu6HeHTJCQzv9T6'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=565455&Code=WNIwA9GN3WFa1TNq7pu6HeHTJCQzv9T6' alt='' style='cursor:pointer' code='WNIwA9GN3WFa1TNq7pu6HeHTJCQzv9T6'></a>`
   public static APP_VERSION = 4
   public static PAGINATE = 24
-  public static MIN_CHARGE = 50000
+  public static MIN_CHARGE = 1000
   public static MIN_WITHDRAW = 100000
   public static CALL_SPEED = 1000
   public static ROOM_REFRESH_TIME = 10
